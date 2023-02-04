b149196ca3fe977d2ebe12ae44e0107d9013e6ad
Alex <73385923+alexemployed@users.noreply.github.com>
2023-02-04 14:17:34 +0400

    Add files via upload

---
+++ ChatRageMpWithVueJS/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <Chat></Chat>
+</template>
+
+<script>
+import ChatRAGE from './components/ChatRAGE.vue';
+export default {
+  name: 'App',
+  components: {
+    Chat: ChatRAGE
+  }
+}
+</script>
